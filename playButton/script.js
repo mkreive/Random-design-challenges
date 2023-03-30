11bf14fe103475b114153a458aff6aec11762be2
mkreive <mkreive@gmail.com>
2023-03-30 09:49:23 +0300

    play button in progress

--- playButton/script.js
+++ playButton/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const playBtn = document.querySelector('.button');
+// const playBtn = document.querySelector('.button__container');
 
-playBtn.addEventListener('click', function (e) {
-    console.log(e.target);
-});
+// playBtn.addEventListener('click', function (e) {
+//     console.log(e.target);
+// });

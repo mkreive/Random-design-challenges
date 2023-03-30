6ea69f5e69e64dfdcf2c9acde387900137f8e2e4
mkreive <mkreive@gmail.com>
2023-03-30 11:23:12 +0300

    play button challenge finished

--- playButton/script.js
+++ playButton/script.js
@@ -2,13 +2,16 @@
 
 const playBtn = document.querySelector('.visually-hidden');
 const label = document.querySelector('.label');
+const audio = document.querySelector('.audio');
 
 playBtn.addEventListener('click', function (e) {
     if (e.target.checked) {
+        audio.play();
         label.innerHTML = 'pause';
         label.classList.remove('move');
         label.classList.add('back');
     } else {
+        audio.pause();
         label.innerHTML = 'play';
         label.classList.add('move');
         label.classList.remove('back');
